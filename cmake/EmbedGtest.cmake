ff9633729c29dc5cb7d1548fed470cda66629600
David Gayerie <dagaydevel@free.fr>
2014-03-20 23:10:22 +0100

    removing pthread support in gtest for cross compilation

--- cmake/EmbedGtest.cmake
+++ cmake/EmbedGtest.cmake
@@ -1,12 +1,15 @@
 include(ExternalProject)
 ###############################################
 # Download and compile gtest
+# Note: pthread support is disabled for cross
+# compilation
 ###############################################
 ExternalProject_Add(
   project_gtest
   URL http://googletest.googlecode.com/files/gtest-1.7.0.zip
   URL_MD5 2d6ec8ccdf5c46b05ba54a9fd1d130d7
   PREFIX "${CMAKE_CURRENT_BINARY_DIR}/gtest"
+  CMAKE_ARGS -Dgtest_disable_pthreads=ON
   INSTALL_COMMAND ""
 )
 
@@ -18,4 +21,3 @@ set_property(TARGET gtest PROPERTY IMPORTED_LOCATION "${BINARY_DIR}/libgtest.a")
 add_dependencies(gtest project_gtest)
 
 include_directories("${SOURCE_DIR}/include")
-
