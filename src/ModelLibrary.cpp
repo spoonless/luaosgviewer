155932d217806c6cdc6705ed5522aae7c5c14396
David Gayerie <dagaydevel@free.fr>
2014-04-04 00:08:43 +0200

    enabling cache support for loading file

--- src/ModelLibrary.cpp
+++ src/ModelLibrary.cpp
@@ -11,6 +11,8 @@ binding::Model::Model(const Model &model) : _node(model._node)
 
 binding::Model* binding::ModelLoader::operator()(const char* filename)
 {
-    osg::Node *node = osgDB::readNodeFile(filename);
+    osg::ref_ptr<osgDB::Options> options = new osgDB::Options;
+    options->setObjectCacheHint(osgDB::Options::CACHE_ALL);
+    osg::Node *node = osgDB::readNodeFile(filename, options);
     return node == 0 ? 0 : new binding::Model(node);
 }
