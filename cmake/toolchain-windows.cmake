8e5e45d158b9af609565848fced1cb5ac6825196
david <dagaydevel@sourceforge.net>
2014-04-14 01:47:42 +0200

    adding better support for cmake and cross compilation

--- cmake/toolchain-windows.cmake
+++ cmake/toolchain-windows.cmake
@@ -9,7 +9,7 @@ set(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
 set(CMAKE_RC_COMPILER i586-mingw32msvc-windres)
 
 # here is the target environment located
-set(CMAKE_FIND_ROOT_PATH /usr/i586-mingw32msvc/)
+set(CMAKE_FIND_ROOT_PATH /usr/i586-mingw32msvc/ "$ENV{OSGDIR}" "$ENV{OSG_DIR}" "$ENV{OSG_ROOT}")
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
